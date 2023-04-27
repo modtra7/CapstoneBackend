8af2df9c697e835e46f9876db18624d3c848eb05
momot <motraore611@yahoo.com>
2023-04-27 15:26:33 -0400

    block breaker game setup

---
+++ CodeBreaker/Game.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Breaker 
+{
+    public class Game
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        public void GameTimer(object sender, EventArgs e)
+        {
+
+        }
+
+        public void keyIsDown(object sender, EventArgs e)
+        {
+
+        }
+
+        public void KeyIsUp(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
