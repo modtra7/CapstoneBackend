261e8dc3c023abb222601b5d6b02a0e5908a9e03
momot <motraore611@yahoo.com>
2023-04-27 15:53:27 -0400

    update

--- CodeBreaker/Game.cs
+++ CodeBreaker/Game.cs
@@ -4,12 +4,42 @@ namespace Breaker
 {
     public class Game
     {
+
+        bool goLeft;
+        bool goRight;
+        bool isGameOver;
+
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+
+        Random random = new Random();
         public Game()
         {
             InitializeComponent();
         }
 
-        public void GameTimer(object sender, EventArgs e)
+        public void gameSetup()
+        {
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 12;
+            txtScore.Text = "Score: " + score;
+
+            gameTimer.Start();
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is Box && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                }
+            }
+        }
+
+        public void gameTimerEvent(object sender, EventArgs e)
         {
 
         }
@@ -21,7 +51,7 @@ namespace Breaker
 
         public void KeyIsUp(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
